912750a7d4e0c47009f24ea44c02fbc0c14d7ae8
Dmitriy Parkhomenko <parkhomenko@Dmitriys-MacBook-Pro.local>
2017-07-27 15:24:39 +0300

    Created Observable by create method

--- main.ts
+++ main.ts
@@ -2,7 +2,13 @@ import { Observable, Observer } from 'rxjs';
 
 let numbers = [ 1,2,5,19 ]
 
-let source = Observable.from(numbers);
+let source = Observable.create(observer => {
+    for (let n of numbers) {
+        observer.next(n);
+    }
+
+    observer.complete();
+});
 
 source.subscribe(
     value => console.log(`value is ${value}`),
