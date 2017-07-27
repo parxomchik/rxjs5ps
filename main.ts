12926b98edfb2d08a0bef7f823fca44de9bbd572
Dmitriy Parkhomenko <parkhomenko@Dmitriys-MacBook-Pro.local>
2017-07-27 15:18:59 +0300

    Refactored observer

--- main.ts
+++ main.ts
@@ -4,19 +4,8 @@ let numbers = [ 1,2,5,19 ]
 
 let source = Observable.from(numbers);
 
-
-class MyObserver implements Observer<number> {
-    next(value) {
-        console.log(`value is ${value}`);
-    }
-
-    error(e) {
-        console.log(`Error ${e}`);
-    }
-
-    complete() {
-        console.log('compleate');
-    }
-}
-
-source.subscribe(new MyObserver());
+source.subscribe(
+    value => console.log(`value is ${value}`),
+    e =>  console.log(`Error ${e}`),
+    () =>  console.log('compleate')
+);
