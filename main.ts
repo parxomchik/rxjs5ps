42c43c6048abc5dd054120e662c539b96bcc6962
Dmitriy Parkhomenko <parkhomenko@Dmitriys-MacBook-Pro.local>
2017-07-27 15:16:28 +0300

    First  observer is created

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-console.log("ok");
+import { Observable, Observer } from 'rxjs';
+
+let numbers = [ 1,2,5,19 ]
+
+let source = Observable.from(numbers);
+
+
+class MyObserver implements Observer<number> {
+    next(value) {
+        console.log(`value is ${value}`);
+    }
+
+    error(e) {
+        console.log(`Error ${e}`);
+    }
+
+    complete() {
+        console.log('compleate');
+    }
+}
+
+source.subscribe(new MyObserver());
